4ce071667c82041bd2e8861d7aca8d2de19c61c6
Franco Rootman <franco.rootman@mmltd.co.za>
2021-04-16 14:57:21 +0200

    Added BASIC toolbar and footer shared components

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { CoreModule } from './core/core.module'
-import { CommonModule } from '@angular/common'
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {CoreModule} from './core/core.module';
+import {SharedModule} from './shared/shared.module';
+import {CommonModule} from '@angular/common';
 
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,9 +18,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CommonModule,
     AppRoutingModule,
     CoreModule,
+    SharedModule,
     BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- web/src/app/shared/shared.module.ts
+++ web/src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ToolbarComponent, FooterComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ToolbarComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
